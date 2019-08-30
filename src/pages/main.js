33e764379b715a96430bb717d641170a64421f66
Administrator <ramanrhiya@gmail.com>
2019-08-30 16:07:59 +0530

    Hello Rhiya!

---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Mainpage extends React.Component{
+	render(){
+		return( <p>Name</p>
+                        <p>College Name</p>
+                        <p>Section</p>
+                        <p>Department</p>
+		)
+	}
+}
